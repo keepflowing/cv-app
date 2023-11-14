2fc531ec3b7a2b9462e0b34727f817380bffa93c
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-11-14 17:20:06 -0500

    Finish dropdown for fonts

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
-const changeFont = () => {
+const changeFont = (font) => {
   const r = document.querySelector(':root');
-  r.style.setProperty('--cv-font', 'Arial');
+  r.style.setProperty('--cv-font', font);
 }
 
 function DropDown() {
   return (
     <div className="dropDown">
-      <p>Hello World</p>
+      <a onClick={() => changeFont('Arial, sans-serif')}>Arial</a>
+      <a onClick={() => changeFont('Comic Sans MS, Comic Sans, cursive')}>Comic Sans</a>
+      <a onClick={() => changeFont('JetBrains Mono')}>JetBrains Mono</a>
+      <a onClick={() => changeFont('Times New Roman, Times, serif')}>Times New Roman</a>
+      <a onClick={() => changeFont('Roboto')}>Roboto</a>
     </div>
   )
 }
@@ -18,8 +22,10 @@ export default function TopBar({onReset, onExample}) {
 
   return (
     <nav id='topBar'>
-      <button onClick={() => setOpen(!open)}>Font</button>
-      {open && <DropDown/>}
+      <button onClick={() => setOpen(!open)}>
+        Font
+        {open && <DropDown/>}
+      </button>
       <button onClick={window.print}>Print</button>
       <h1>Create CV</h1>
       <button onClick={onReset}>Reset</button>
