c8dc5cc81e890b1893d767055037685099521f44
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-11-13 23:10:25 -0500

    WIP, dropdown

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
+const changeFont = () => {
+  const r = document.querySelector(':root');
+  r.style.setProperty('--cv-font', 'Arial');
+}
+
+function DropDown() {
+  return (
+    <div className="dropDown">
+      <p>Hello World</p>
+    </div>
+  )
+}
+
 export default function TopBar({onReset, onExample}) {
+  const [open, setOpen] = useState(false)
+
   return (
     <nav id='topBar'>
-      <button>Font</button>
+      <button onClick={() => setOpen(!open)}>Font</button>
+      {open && <DropDown/>}
       <button onClick={window.print}>Print</button>
       <h1>Create CV</h1>
       <button onClick={onReset}>Reset</button>
